51ec2faeaa57d4b58da86582cd360ec93536dc30
wolfy1339 <webmaster@wolfy1339.com>
2019-10-30 22:08:16 -0400

    Add missinf semicolons

--- src/constants.ts
+++ src/constants.ts
@@ -2,8 +2,8 @@
  * License, v. 2.0. If a copy of the MPL was not distributed with this
  * file, You can obtain one at https://mozilla.org/MPL/2.0/. */
 
-const INIT = Symbol('INIT')
-const RESUME = Symbol('RESUME')
+const INIT = Symbol('INIT');
+const RESUME = Symbol('RESUME');
 
 const constants = {
   ConnectionModes: {
